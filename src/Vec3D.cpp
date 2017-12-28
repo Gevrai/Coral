2b6211856e0470a4c179da32d0482e17f40c6547
Gevrai J-Tremblay <gevrai@gmail.com>
2017-12-28 03:20:08 -0500

    More functionnalities for Vec3D
    - Functions named "InPlace" change the subject vector directly

--- src/Vec3D.cpp
+++ src/Vec3D.cpp
@@ -4,7 +4,26 @@ Vec3D::Vec3D(double x, double y, double z)
 	: x(x), y(y), z(z)
 {}
 
-void Vec3D::normalize() {
+void Vec3D::operator()(double _x, double _y, double _z) {
+	x = _x; y = _y; z = _z;
+}
+
+Vec3D Vec3D::operator*(double ammount) const {
+	return Vec3D(x*ammount, y*ammount, z*ammount);
+}
+
+void Vec3D::addInPlace(Vec3D that)
+{
+	x += that.x; y += that.y; z += that.z;
+}
+
+void Vec3D::multInPlace(double ammount)
+{
+	x *= ammount; y *= ammount; z *= ammount;
+}
+
+void Vec3D::normalize()
+{
 	//Cannot normalize a nil vector
 	if (x == 0.0 && y == 0.0 && z == 0.0)
 		return;
@@ -13,6 +32,7 @@ void Vec3D::normalize() {
 	y /= norm;
 	z /= norm;
 }
+
 double Vec3D::norm() const
 {
 	return std::sqrt(x*x + y*y + z*z);
--- src/Vec3D.h
+++ src/Vec3D.h
@@ -5,7 +5,13 @@ class Vec3D {
 	double x,y,z;
 	Vec3D(double x, double y, double z);
 
+	void operator()(double x, double y, double z);
+	Vec3D operator*(double ammount) const;
+
 	void normalize();
+	void addInPlace(Vec3D that);
+	void multInPlace(double ammount);
+
 	double norm() const;
 	double dot(Vec3D that) const;
 	Vec3D cross(Vec3D that) const;
